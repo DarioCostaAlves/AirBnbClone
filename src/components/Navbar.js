d7f2151efe28e024c13ae43c3b1cfda4811f42aa
DarioCostaAlves <dario.c.alves22@gmail.com>
2022-05-03 00:06:07 +0100

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import airbnb from '../assets/airbnb.png'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={airbnb} />
+        </nav>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import App from './App'
+import '../src/style.css'
+
+ReactDOM.render(<App />, document.getElementById("root"))
