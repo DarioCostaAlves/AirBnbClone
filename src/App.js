9819c19c27f98ab431607e82ef8142045be0ceb8
DarioCostaAlves <dario.c.alves22@gmail.com>
2022-05-08 23:18:00 +0100

    Card example. Hard Coded data.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Card from './components/Card'
 
 export default function App() {
     return (
         <div>
             <Navbar />
             <Hero />
+            <Card />
         </div>
     )
 }
